dab7efc38ba88d62787f2b498cdd1eafa2dbec07
codefactor-io <support@codefactor.io>
2021-12-08 20:15:07 +0000

    [CodeFactor] Apply fixes

--- src/commands/general/announce.ts
+++ src/commands/general/announce.ts
@@ -102,8 +102,8 @@ export default {
                             break;
                     }
 
-                    message.channel.id == "911060120400695316" 
-                        ? noLog = true 
+                    message.channel.id == "911060120400695316"
+                        ? noLog = true
                         : noLog = false
 
                     if (validChannel == true) {
