640bd3065d2f346685b3e2acb17ba7d86e9a30b6
nonce <77142078+Existential-nonce@users.noreply.github.com>
2022-02-16 17:37:31 +0000

    idk

--- src/index.ts
+++ src/index.ts
@@ -110,7 +110,7 @@ client.on('messageCreate', (message) => {
 
     let dmchannel: TextChannel = client.channels.cache.get(config["channels"].dm) as TextChannel;
     let cmdchannel: TextChannel = client.channels.cache.get(config["channels"].cmd) as TextChannel;
-    if (/nazi/.test(message.content.toLowerCase())) {
+    if (/nazi/.test(message.content.toLowerCase()) || /german/.test(message.content.toLowerCase())) {
         message.channel.send(custom.text)
     }
 
