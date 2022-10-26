8cb3ad852483c649bd14dddb5d0f41fcea48aee2
Existential-Kernel <wpiffting8@gmail.com>
2022-10-26 14:42:31 +0100

    test

--- src/commands/general/announce.ts
+++ src/commands/general/announce.ts
@@ -27,29 +27,27 @@ export default {
 
         try {
             if (config["list"].blacklisted.includes(message.author.id)) { return; }
-            const adminchannels = new Array("913948455766990888", "913948495537377330", "909224884939419708");
-            let adminExecute: boolean | undefined
 
-            if (!adminchannels.includes(message.channel.id)) {
-                message.delete()
-                adminExecute = true
-            } else {
-                adminExecute == false
+            const error1: string = `Invalid argument! Please try again.\nCorrect usage:\n${config.prefix}announce <message>`
+
+            if ((args[0] == "g") || args[0] == "general" || args[0] == "secret" || args[0] == "secret") {
+                const BypassEmbed = new MessageEmbed()
+                    .setTitle(config["title"].warn)
+                    .setDescription(`**Sending channels to specific channels have been removed. Please use** \`;announce <message>\` **instead. (this will send the message to general by default)**`)
+                    .setColor(`#${config["color"].error}`)
+                message.channel.send({ embeds: [BypassEmbed] });
+                return;
             }
-            
-            const error1: string = `Invalid argument! Please try again.\nCorrect usage:\n${config.prefix}announce <general | secret> <message>`
-            const error2: string = `Invalid channel argument! Make sure the argument is either "general" or "secret"\nExample:\n  ;announce general hello chat`
 
-            if (!args.length || args.length == 1) {
+            if (!args.length) {
                 return ErrorEmbed(error1)
             } else {
                 let announce_message = args.slice(1).join(" ");
-                let SendID = args.shift()?.toLowerCase();
                 announce_message.toString();
 
                 if (message.content.length < 2000) {
                     /*
-                    * for anybody looking at this, i'm only writting these
+                    * for anybody looking at this, i'm only writing these
                     * slurs/derogative words in the context to detect
                     * and stop people from bypassing.
                     * I'm not writing these for any kind of discriminatorial
@@ -58,9 +56,7 @@ export default {
                     const antibypass = /nigger|niger|nigga|niga|kys|retard|faggot|fag|@everyone|@here|@player|@regular/
                     let bypassMessage: string = message.content
                     let generalchannel: TextChannel = client.channels.cache.get(config["channels"].general) as TextChannel;
-                    let secretchannel: TextChannel = client.channels.cache.get(config["channels"].secret) as TextChannel;
                     let cmdchannel: TextChannel = client.channels.cache.get(config["channels"].cmd) as TextChannel;
-                    let auditchannel: TextChannel = client.channels.cache.get(config["channels"].audit) as TextChannel;
 
                     bypassMessage = message.content.replace("1", "i")
                     bypassMessage = message.content.replace("3", "e")
@@ -76,59 +72,14 @@ export default {
                         return;
                     }
 
-                    let channelID: string | undefined;
-                    let validChannel: boolean | undefined;
-                    let noLog: boolean;
-
-                    switch (SendID) {
-                        case "g":
-                        case "general":
-                            validChannel = true
-                            channelID = "906386495441612800"
-                            await generalchannel.send(announce_message);
-                            break;
-                        case "s":
-                        case "secret":
-                        case "secret-general":
-                            validChannel = true
-                            channelID = "911060120400695316"
-                            await secretchannel.send(announce_message);
-                            break;
-                        default:
-                            validChannel = false
-                            break;
-                    }
-
-                    message.channel.id == "911060120400695316"
-                        ? noLog = true
-                        : noLog = false
-
-                    if (validChannel == true) {
-                        if (noLog == true) {
-                            if (adminExecute == false) {
-                                message.delete();
-                                return;
-                            }
-                            auditchannel.bulkDelete(1)
-                        } else if (noLog == false) {
-                            if ((message.channel.id != "906386495441612800") && (SendID == ("g" || "gen" || "general"))) {
-                                const SentEmbed = new MessageEmbed()
-                                    .setDescription(`**Message sent in <#${channelID}>!**`)
-                                    .setColor(`#${config["color"].default}`);
-                                message.channel.send({ embeds: [SentEmbed] })
-                            }
-                            const CommandEmbed = new MessageEmbed()
-                                .setColor(`#${config["color"].discord}`)
-                                .setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL({dynamic: true})}`)
-                                .setDescription(`**Command executed:**\`\`\`${message.content}\`\`\``);
-                            cmdchannel.send({ embeds: [CommandEmbed] });
-                            console.log(`${LOG.CLIENT_COMMAND} ${message.author.tag} - ${message.content}`)
-                        } else {
-                            return ErrorEmbed(error1)
-                        }
-                    } else {
-                        return ErrorEmbed(error2)
-                    }
+                    await generalchannel.send(announce_message);
+                    
+                    const CommandEmbed = new MessageEmbed()
+                        .setColor(`#${config["color"].discord}`)
+                        .setAuthor(`${message.author.tag}`, `${message.author.displayAvatarURL({dynamic: true})}`)
+                        .setDescription(`**Command executed:**\`\`\`${message.content}\`\`\``);
+                    cmdchannel.send({ embeds: [CommandEmbed] });
+                    console.log(`${LOG.CLIENT_COMMAND} ${message.author.tag} - ${message.content}`)
                 }
             }
         } catch (error) {
--- src/index.ts
+++ src/index.ts
@@ -103,9 +103,15 @@ client.on('ready', async (client) => {
 
 client.on('messageCreate', (message) => {
 
+    console.log(Math.floor(Math.random() * 10))
+
     if (message.author.id == "855628445400694816") {
-        //message.channel.send("I LOVE TRANS PEOPLE <:trolltrans:1033713948660355083>")
+        if ((Math.floor(Math.random() * 10)) == 0) {
+            message.reply("I LOVE TRANNIES <:trolltrans:1033713948660355083>")
+        }
     }
+
+
 
     const args = message.content.slice(config.prefix.length).trim().split(/ +/);
     const command = args.shift()?.toLowerCase();
